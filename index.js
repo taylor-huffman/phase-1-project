405d6d84e0475fb322725acab8fbf81ae4e9b4ca
Taylor Huffman <taylor@crucialartists.com>
2022-02-14 09:53:03 -0600

    Add global variables

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ const fetchActivity = () => {
 }
 
 fetchActivity()
+
+
+const navbarLogo = document.getElementById('navbar-logo')
+const statsIcon = document.getElementById('icon-stats-graph')
+const contentSection = document.getElementById('content')
+const tryButtonContainer = document.getElementById('try-button-container')
+const tryButton = document.getElementById('try-button')
+const form = document.getElementById('submit-name')
+const formContainer = document.getElementById('name-prompt-container')
