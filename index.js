eddfca5350740471e7e1aad7954fe22ce5c4c347
Taylor Huffman <taylor@crucialartists.com>
2022-02-14 10:02:08 -0600

    Add showactivity function and eventlistener to suggest an activity button

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const fetchActivity = () => {
     fetch('https://www.boredapi.com/api/activity/')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => showActivity(data))
 }
-
-fetchActivity()
 
 
 const navbarLogo = document.getElementById('navbar-logo')
@@ -14,3 +12,30 @@ const tryButtonContainer = document.getElementById('try-button-container')
 const tryButton = document.getElementById('try-button')
 const form = document.getElementById('submit-name')
 const formContainer = document.getElementById('name-prompt-container')
+
+
+function showActivity(data) {
+    contentSection.innerHTML = ''
+    const activityContainer = document.createElement('div')
+    activityContainer.classList.add('activity-container', 'bg-light', 'py-5', 'px-3', 'my-3', 'shadow')
+    const activityTitle = document.createElement('h2')
+    activityTitle.classList.add('mb-3')
+    activityTitle.textContent = data.activity
+    const activityType = document.createElement('p')
+    activityType.textContent = `Activity Type: ${data.type[0].toUpperCase() + data.type.slice(1)}`
+    const activityParticipants = document.createElement('p')
+    activityParticipants.textContent = `# of Participants: ${data.participants}`
+    const googleLink = document.createElement('a')
+    const googleLinkContainer = document.createElement('p')
+    googleLink.href = `https://www.google.com/search?q=${data.activity}`
+    googleLink.target = '_blank'
+    googleLink.text = 'Learn More'
+    const addButton = document.createElement('button')
+    addButton.classList.add('btn', 'btn-outline-primary', 'mt-2')
+    addButton.textContent = 'Add To My Activities'
+    googleLinkContainer.appendChild(googleLink)
+    activityContainer.append(activityTitle, activityType, activityParticipants, googleLinkContainer, addButton)
+    contentSection.appendChild(activityContainer)
+}
+
+tryButton.addEventListener('click', fetchActivity)
