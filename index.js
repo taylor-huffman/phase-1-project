e4a61d5c03bfcbd4e4ed15b68218eb287705679d
Taylor Huffman <taylor@crucialartists.com>
2022-02-14 13:21:10 -0600

    Add cookie functions, form submission listener/handler function, js code labels, disable/hide classes in stylesheet

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
+
+// Fetch Activity Function
 const fetchActivity = () => {
     fetch('https://www.boredapi.com/api/activity/')
     .then(res => res.json())
     .then(data => showActivity(data))
 }
 
-
+// Global Variables
 const navbarLogo = document.getElementById('navbar-logo')
 const statsIcon = document.getElementById('icon-stats-graph')
 const contentSection = document.getElementById('content')
+const greeting = document.getElementById('greeting')
 const tryButtonContainer = document.getElementById('try-button-container')
 const tryButton = document.getElementById('try-button')
 const form = document.getElementById('submit-name')
 const formContainer = document.getElementById('name-prompt-container')
 const imageArray = ['./media/peep illustration1.svg', './media/peep illustration2.svg', './media/peep illustration3.svg', './media/peep illustration4.svg', './media/peep illustration5.svg', './media/peep illustration6.svg', './media/peep illustration7.svg', './media/peep illustration8.svg']
 
-
+// Reset Content Section (Activity Suggestions) to initial state
 function resetContentSection() {
     contentSection.innerHTML = ''
     contentSection.innerHTML = '<img class="mt-4" src="./media/peep illustration.svg" alt="Goodbye Boredom Logo">'
 }
 
-
+// Show Fetched Activity
 function showActivity(data) {
     contentSection.innerHTML = ''
     const activityContainer = document.createElement('div')
@@ -57,5 +60,59 @@ function showActivity(data) {
 }
 
 
+// Cookie Functions
+function setCookie(cname,cvalue,exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+function getCookie(cname) {
+let name = cname + "=";
+let decodedCookie = decodeURIComponent(document.cookie);
+let ca = decodedCookie.split(';');
+for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+    }
+}
+return "";
+}
+  
+function checkCookie() {
+let user = getCookie("username");
+if (user != "") {
+    formContainer.remove()
+    statsIcon.classList.remove('disabled')
+    greeting.textContent = `Welcome, ${user}!`
+    tryButtonContainer.classList.remove('hide')
+    } else {
+        if (user != "" && user != null) {
+            setCookie("username", user, 90);
+        }
+    }
+}
+
+checkCookie()
+
+// Handles Form Submission 
+function submitHandler(e) {
+    e.preventDefault()
+    setCookie('username', form.name.value, 90)
+    user = getCookie('username')
+    form.reset()
+    formContainer.remove()
+    statsIcon.classList.remove('disabled')
+    greeting.textContent = `Welcome, ${user}!`
+    tryButtonContainer.classList.remove('hide')
+}
+
+// Primary Event listeners
 navbarLogo.addEventListener('click', resetContentSection)
+form.addEventListener('submit', submitHandler)
 tryButton.addEventListener('click', fetchActivity)
