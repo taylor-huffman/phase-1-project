245b4785a3ae94bbf123be9dcc2ba72cc4c3d978
Taylor Huffman <taylor@crucialartists.com>
2022-02-14 10:29:58 -0600

    Modify showactivity function to include pulling random images from image array, add styling for images

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const tryButtonContainer = document.getElementById('try-button-container')
 const tryButton = document.getElementById('try-button')
 const form = document.getElementById('submit-name')
 const formContainer = document.getElementById('name-prompt-container')
+const imageArray = ['./media/peep illustration1.svg', './media/peep illustration2.svg', './media/peep illustration3.svg', './media/peep illustration4.svg', './media/peep illustration5.svg', './media/peep illustration6.svg', './media/peep illustration7.svg', './media/peep illustration8.svg']
 
 
 function showActivity(data) {
@@ -33,8 +34,19 @@ function showActivity(data) {
     const addButton = document.createElement('button')
     addButton.classList.add('btn', 'btn-outline-primary', 'mt-2')
     addButton.textContent = 'Add To My Activities'
+    const activityImagesContainer = document.createElement('div')
+    activityImagesContainer.classList.add('activity-image-container', 'pt-4')
+    const imageLeft = document.createElement('img')
+    const imageRight = document.createElement('img')
+    imageLeft.classList.add('activity-image')
+    imageRight.classList.add('activity-image')
+    imageLeft.id = 'imageLeft'
+    imageRight.id = 'imageRight'
+    imageLeft.src = imageArray[Math.floor(Math.random()*imageArray.length)]
+    imageRight.src = imageArray[Math.floor(Math.random()*imageArray.length)]
     googleLinkContainer.appendChild(googleLink)
-    activityContainer.append(activityTitle, activityType, activityParticipants, googleLinkContainer, addButton)
+    activityImagesContainer.append(imageLeft, imageRight)
+    activityContainer.append(activityTitle, activityType, activityParticipants, googleLinkContainer, addButton, activityImagesContainer)
     contentSection.appendChild(activityContainer)
 }
 
